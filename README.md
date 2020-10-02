aea4abbafdb536f8ad2263cf753a9f4774a5f088
Nigel-Brown <47145130+Nigel-Brown@users.noreply.github.com>
2020-10-02 10:41:46 +0200

    Edited README FILE

---
+++ README.md
@@ -0,0 +1,28 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+<script type="text/javascript">
+code .sourceCode {
+    white-space: pre-wrap;       /* Since CSS 2.1 */
+    white-space: -moz-pre-wrap;  /* Mozilla, since 1999 */
+    white-space: -pre-wrap;      /* Opera 4-6 */
+    white-space: -o-pre-wrap;    /* Opera 7 */
+    word-wrap: break-word;       /* Internet Explorer 5.5+ */
+    overflow-wrap: break-word;
+}
+</script>
+
+# HarvardX PH125.9x CYOP Submission
+
+This is a repo for the HarvardX PH125.9x Chose Your Own Project
+Submission by Nigel-Brown.
+
+The submission for this project is three files:
+
+1.  A report in PDF format
+2.  A report in Rmd format
+3.  A script in R format that performs a supervised machine learning
+    task
+
+This files may be found in the directory titled Submission. The dataset
+utilized in this project is located in the data directory.
---
+++ code/data_prep.R
@@ -0,0 +1,50 @@
+library(tidyverse)
+library(lubridate)
+
+# https://www.gov.uk/guidance/about-the-price-paid-data
+# If you use or publish our Price Paid Data, you must add the following attribution statement:
+# Contains HM Land Registry data © Crown copyright and database right 2020. This data is licensed under the Open Government Licence v3.0.
+# http://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/
+
+url = 'http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com/pp-complete.txt'
+
+data = read_csv(url, col_names = c("trans_id", "price", "trans_date", "postcode",
+                                   "property_type", "new_build", "estate_type", "paon", "saon",
+                                   "street", "locality", "town", "district", "county", "cat_type", "record_status"))
+
+greater_london_districts <- c("BARKING AND DAGENHAM","BARNET", "BEXLEY", "BRENT", "BROMLEY",
+                              "CAMDEN", "CROYDON", "EALING", "ENFIELD", "GREENWICH", "HACKNEY",
+                              "HAMMERSMITH AND FULHAM", "HARINGEY", "HARROW", "HAVERING", "HILLINGDON",
+                              "HOUNSLOW", "ISLINGTON", "KENSINGTON AND CHELSEA", "KINGSTON", "LAMBETH",
+                              "LEWISHAM", "MERTON", "NEWHAM", "REDBRIDGE", "RICHMOND", "SOUTHWARK", "SUTTON",
+                              "TOWER HAMLETS", "WALTHAM FOREST", "WANDSWORTH", "WESTMINISTER")
+
+data$saon <- ifelse(is.na(data$saon), "", data$saon)
+
+lhd <- data %>% filter(district %in% greater_london_districts & cat_type == "A" & estate_type == "F") %>% 
+  mutate(trans_date = ymd(trans_date),
+         address = paste0(paon , saon ,' ', street)) %>% 
+  select(-c('cat_type', 'town', 'county', 'paon', 'saon', 'street', 'locality', 'record_status', 'estate_type', 'trans_id'))
+
+
+
+#  https://www.ons.gov.uk/methodology/geography/licences
+# You may re-use this information (not including logos or Northern Ireland data) free of charge in any format or medium, under the terms of the relevant # # data owners' licence. 
+# In addition, the following attribution statements must be acknowledged or displayed whenever the owners data is used:
+
+# Contains Ordnance Survey data © Crown copyright and database right 2020
+
+# Contains Royal Mail data © Royal Mail copyright and database right 2020
+
+# Source: Office for National Statistics licensed under the Open Government Licence v.3.0
+
+url <- "https://www.freemaptools.com/download/full-postcodes/ukpostcodes.zip"
+tmp <- tempfile()
+download.file(url, tmp)
+pc <- read_csv(unz(tmp, 'ukpostcodes.csv'))
+pc <- pc %>% select(-id)
+lhd <- lhd %>% 
+  left_join(pc, by = 'postcode')
+head(lhd)
+
+write_rds(lhd, here::here('data', 'lhd.rds'), compress = "xz")
