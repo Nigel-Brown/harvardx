0c257e33bb00745a64c2b4890a1aa37a15b05eb6
Nigel-Brown <47145130+Nigel-Brown@users.noreply.github.com>
2020-11-11 15:11:34 +0100

    code update

--- housing_prices.R
+++ housing_prices.R
@@ -1,3 +1,6 @@
+# Load library packages ---------------------------------------------------
+
+
 repos <- 'https://cran.rstudio.com/'
 
 if(!require(tidyverse)) install.packages("tidyverse", repos = repos)
@@ -7,7 +10,7 @@ if(!require(leaflet)) install.packages("leaflet", repos = repos)
 if(!require(lubridate)) install.packages("lubridate", repos = repos)
 if(!require(tictoc)) install.packages("tictoc", repos = repos)
 
-# clean up 
+## clean up 
 rm(repos)
 
 library(tidyverse)
@@ -17,27 +20,26 @@ library(lubridate)
 library(leaflet)
 library(tictoc)
 
-# set theme for charts
+# Set theme for plots ---------------------------------------------------
 theme_set(theme_minimal())
 
 
-# load the dataset lhd
+# Load the dataset lhd ---------------------------------------------------
 df <- read_csv(here::here('data', 'kc_house_data.csv'))
 
-## data wrangling
+# Data wrangling ---------------------------------------------------
 
-# remove features with a predominant number of zeros
-status <- df_status(df, print_results = FALSE)
+# Remove features with a predominant number of zeros ---------------------------------------------------
 remove_vars <-  status %>% filter(status$p_zeros > 0.6) %>% pull(variable)
 df <- df %>% select(-one_of(remove_vars))
 
-# create year and month columns
+# Create year and month columns ---------------------------------------------------
 df <- df %>% 
   mutate(year = year(date),
          month = month(date)) %>% 
   select(-date)
 
-# convert data format to integers
+# Convert data format to integers ---------------------------------------------------
 to_convert <- df %>% 
   select(-c('id', 'bathrooms', 'lat', 'long')) %>% 
   colnames()
@@ -45,17 +47,15 @@ to_convert <- df %>%
 df <- df %>% 
   mutate_at(to_convert, as.integer)
 
-# remove anomolous data
+# Remove anomolous data ---------------------------------------------------
 df <- df %>% 
   filter(bedrooms != 0) %>% 
   mutate(bedrooms = replace(bedrooms, bedrooms==33, 3))
 
-# clean update memory
+## Clean up 
 rm(remove_vars, status, to_convert)
 
-library(leaflet)
-
-## create map plots for the RMD file
+# Create map plots for the RMD file ---------------------------------------------------
 
 # create a palette for the map
 bed_pal <- colorFactor(c("#000080", "red"), 1:2)
@@ -105,15 +105,15 @@ mapshot(m, file = here::here('images', '5plus_beds.png'))
 # clean up memory
 rm(m, bed_pal)
 
-# Convert price to a log10 value
+# Convert price to a log10 value ---------------------------------------------------
 df <- df %>% mutate(price = log10(price))
 
 
-# Set the random number stream using `set.seed()` so that the results can be 
+# Set the random number seed using `set.seed()` so that the results can be 
 # reproduced later. 
 set.seed(54321)
 
-# Save the split information for an 80/20 split of the data, stratifying on price
+# Save the split for an 80/20 split of the data, stratifying on price ---------------------------------------------------
 df_split <- initial_split(df, prob = 0.8, strata = price)
 
 df_train <- training(df_split)
@@ -122,10 +122,10 @@ df_test  <-  testing(df_split)
 write_rds(df_train, here::here('data', 'train.rds'))
 write_rds(df_train, here::here('data', 'test.rds'))
 
-# create CV object from training data
+# Create cross validation object from training data ---------------------------------------------------
 df_cv <- vfold_cv(df_train)
 
-# create lm recipe
+# Create lm recipe ---------------------------------------------------
 df_rec <- 
   recipe(price ~ ., data = df_train) %>%
   update_role(id, new_role = "ID") %>%
@@ -133,20 +133,19 @@ df_rec <-
   step_log(starts_with("sqft_"), base = 10) %>% 
   prep()
 
-df_rec
-
-# create model
+# Create lm model ---------------------------------------------------
 lm_mod <- linear_reg() %>% 
   set_engine("lm")
 
-# create workflow
+# Create lm workflow ---------------------------------------------------
 lm_wflow <- workflow() %>% 
   add_model(lm_mod) %>% 
   add_recipe(df_rec)
 
+
+# Fit lm model ---------------------------------------------------
 # Tic Toc used to time the model fitting
 tic()
-
 lm_fit <- 
   lm_wflow %>% 
   fit(data = df_train)
@@ -154,17 +153,13 @@ lm_fit <-
 lm_fit %>% 
   pull_workflow_fit() %>% 
   tidy()
-
 toc()
 
-
+# Predict price using the fitted lm model and visualize---------------------------------------------------
 lm_test_res <- 
   predict(lm_fit, new_data = df_test %>% select(-price)) 
 
 lm_test_res <- bind_cols(lm_test_res, df_test %>% select(price))
-
-
-lm_test_res
 
 lm_test_res %>% 
   ggplot(aes(price, .pred)) + 
@@ -175,13 +170,20 @@ lm_test_res %>%
   # Scale and size the x- and y-axis uniformly:
   coord_obs_pred()
 
+# Capture results RMSE & R^2 ---------------------------------------------------
+res_metrics <- metric_set(rmse, rsq)
+results <- res_metrics(lm_test_res, truth = price, estimate = .pred)
+rmse <- results %>% filter(results$.metric == 'rmse') %>% select(.estimate)
+rsq <- results %>% filter(results$.metric == 'rsq') %>% select(.estimate)
 
-res_metrics <- metric_set(rmse, rsq, mae)
-res_metrics(lm_test_res, truth = price, estimate = .pred)
+result <- tibble(Method = "Linear Regression Model", RMSE = rmse$.estimate, RSQ = rsq$.estimate)
 
-## Random Forest model
+# clean up
+rm(lm_fit, lm_mod, lm_wflow)
 
-rf_spec <- 
+# Random Forest model ---------------------------------------------------
+
+# Create rf spec ---------------------------------------------------
   rand_forest(
     mtry = tune(),
     trees = 1000,
@@ -189,16 +191,17 @@ rf_spec <-
   set_engine("ranger") %>% 
   set_mode("regression")
 
-
+# Create rf workflow ---------------------------------------------------
 rf_wflow <- 
   workflow() %>% 
   add_model(rf_spec) %>% 
   add_recipe(df_rec)
 
+
+# Tune hyperparameters ---------------------------------------------------
 # The tuning takes a while go get a coffee !
 tic()
 doParallel::registerDoParallel()
-
 set.seed(123)
 tune_res <- tune_grid(
   rf_wflow,
@@ -209,6 +212,8 @@ tune_res <- tune_grid(
 write_rds(tune_res, here::here('data', 'tune_res.rds'))
 toc()
 
+
+# Hyperparameter tuning results ---------------------------------------------------
 tune_res %>%
   collect_metrics() %>%
   filter(.metric == "rsq") %>%
@@ -222,22 +227,16 @@ tune_res %>%
   facet_wrap(~parameter, scales = "free_x") +
   labs(x = NULL, y = "RSQ")
 
+# Hyperparameter 2nd tuning using a regular grid ---------------------------------------------------
 rf_grid <- grid_regular(
   mtry(range = c(4, 20)),
   min_n(range = c(2, 10)),
   levels = 5
 )
 
-rf_grid
-
-# Clean up
-
-rm(tune_res, rf_fit)
-
-
-# Last tuning of hyperparameters
 set.seed(456)
 
+# Tuning takes a while, be patient
 tic()
 doParallel::registerDoParallel()
 
@@ -251,13 +250,13 @@ toc()
 best_rmse <- select_best(regular_res, "rmse")
 write_rds(best_rmse, here::here('data', 'best.rds'))
 
-# final model using best hyperparameters
+# Final model using best hyperparameters -------------------------------------
 final_rf <- finalize_model(
   rf_spec,
   best_rmse
 )
 
-library(vip)
+# Random Forest variable importance ----------------------------------------
 tic()
 final_rf_vip <- final_rf %>%
   set_engine("ranger", importance = "permutation") %>%
@@ -279,7 +278,7 @@ ggsave(vip_gg, file = here::here('images', 'vip.png'))
 rm(vip_gg)
 toc()
 
-# Final workflow for random forest
+# Final workflow for random forest ------------------------------------
 final_wf <- workflow() %>%
   add_recipe(df_rec) %>%
   add_model(final_rf)
@@ -288,15 +287,18 @@ final_wf <- workflow() %>%
 # Model is fit against the original split, 
 # takes roughly a minute to run
 tic()
-final_res <- final_wf %>%
+rf_res <- final_wf %>%
   last_fit(df_split)
 toc()
 
-final_res <-  final_res %>%  collect_metrics()
+rmse <- rf_res %>% filter(rf_res$.metric == 'rmse') %>% select(.estimate)
+rsq <- rf_res %>% filter(rf_res$.metric == 'rsq') %>% select(.estimate)
 
-write_rds(final_res, here::here('data', 'final.rds'))
+result <- bind_rows(result, tibble(Method = "Random Forest Model",  RMSE = rmse$.estimate, RSQ = rsq$.estimate))
 
-# XGBoost Specification
+write_rds(rf_res, here::here('data', 'rf_res.rds'))
+
+# XGBoost Specification ----------------------------------
 xgb_spec <- boost_tree(
   trees = 1000, 
   tree_depth = tune(), 
@@ -310,7 +312,7 @@ xgb_spec <- boost_tree(
   set_mode("regression")
 
 
-# XGBoost tuning grid
+# XGBoost tuning grid -----------------------------------
 xgb_grid <- grid_latin_hypercube(
   tree_depth(),
   min_n(),
@@ -321,7 +323,7 @@ xgb_grid <- grid_latin_hypercube(
   size = 30
 )
 
-# XGBoost workflow
+# XGBoost workflow -------------------------------------
 xgb_wf <- workflow() %>%
   add_formula(price ~ bedrooms + bathrooms + sqft_living + sqft_lot +
                 floors + condition + grade + sqft_above + yr_built + zipcode + lat +
@@ -334,7 +336,7 @@ xgb_wf <- workflow() %>%
 tic()
 doParallel::registerDoParallel()
 
-set.seed(234)
+set.seed(12345)
 xgb_res <- tune_grid(
   xgb_wf,
   resamples = df_cv,
@@ -352,18 +354,18 @@ write_rds(best_rsq, here::here('data', 'xgb_best_rsq.rds'))
 toc()
 
 
-# Final xgb workflow
-final_xgb <- finalize_workflow(
+# Final xgb workflow ------------------------------------------
+final_xgb_wf <- finalize_workflow(
   xgb_wf,
   best_rsq
 )
 
 
-# Variable importance of the xgboost model
+# Variable importance of the xgboost model -----------------------------------
 tic()
 doParallel::registerDoParallel()
 
-final_xgb_vip <- final_xgb %>%
+final_xgb_vip <- final_xgb_wf %>%
   fit(data = df_train) %>%
   pull_workflow_fit() 
 toc()
@@ -381,6 +383,19 @@ ggsave(xgb_vip_gg, file = here::here('images', 'xgb_vip.png'))
 
 
 rm(xgb_vip_gg )
+
+# run the model using the initial split, the results will be evaluated against the test set.
+tic()
+final_res <- last_fit(final_xgb, df_split)
+toc()
+xgb_res <- collect_metrics(final_res)
+rmse <- xgb_res %>% filter(xgb_res$.metric == 'rmse') %>% select(.estimate)
+rsq <- xgb_res %>% filter(xgb_res$.metric == 'rsq') %>% select(.estimate)
+
+result <- bind_rows(result, tibble(Method = "XGBoost Model",  RMSE = rmse$.estimate, RSQ = rsq$.estimate))
+
+write_rds(xgb_res, here::here('data', 'xgb_res.rds'))
+
  
 
 
